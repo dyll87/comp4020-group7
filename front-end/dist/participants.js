8c81fb2628b89e66c52d96437d77a67ad56deda8
charlesAOC <86214151+charlesAOC@users.noreply.github.com>
2025-03-14 19:12:56 -0500

    clean up (#30)

--- front-end/dist/participants.js
+++ front-end/dist/participants.js
@@ -1,50 +1,42 @@
 import { InitializeList } from "./components/list.js";
 import { mountPageWrapper } from "./components/pageWrapper.js";
-import { TemplateItem, Participants } from "./types/types.js";
-
+import { TemplateItem } from "./types/types.js";
 const IS_INDEX_PAGE = false;
-const IS_EXPANDABLE = true;
+const IS_EXPANDABLE = false;
 const showSuggestedButton = false;
-const actionButtonType = "default";
-
-// Mount the page wrapper 
+const actionButtonType = "delete";
+// Mount page wrapper 
 mountPageWrapper({
-  title: "Participants",
-  isIndexPage: IS_INDEX_PAGE,
-  onAddClick: () =>
-    list.addItem({
-      item: TemplateItem,
-      expandable: IS_EXPANDABLE,
-      list,
-      actionButtonType,
-    }),
-  showSuggested: showSuggestedButton,
+    title: "Participants",
+    isIndexPage: IS_INDEX_PAGE,
+    onAddClick: () => {
+        // Prompt the user to enter a participant name.
+        const participantName = prompt("Enter participant name:");
+        if (participantName && participantName.trim() !== "") {
+            const newParticipant = Object.assign({}, TemplateItem);
+            newParticipant.label = participantName;
+            newParticipant.description = "Participant added by user";
+            newParticipant.categoryID = "participant";
+            list.addItem({
+                item: newParticipant,
+                expandable: IS_EXPANDABLE,
+                list,
+                actionButtonType,
+                showInputDefault: false,
+            });
+        }
+    },
+    showSuggested: showSuggestedButton,
 });
-
 // Initialize the list 
 const list = InitializeList({
-  onAddItem: (item) => {
-    console.log("item added...", item);
-  },
-  ondeleteItem: (itemID) => {
-    console.log("item deleted...", itemID);
-  },
-  onupdateItem: (item) => {
-    console.log("item updated...", item);
-  },
+    onAddItem: (item) => {
+        console.log("item added...", item);
+    },
+    ondeleteItem: (itemID) => {
+        console.log("item deleted...", itemID);
+    },
+    onupdateItem: (item) => {
+        console.log("item updated...", item);
+    },
 });
-
-// Add the list of participants 
-Participants.slice()
-  .reverse()
-  .forEach((participant) => {
-    const template = { ...TemplateItem }; 
-    template.label = participant;
-    list.addItem({
-      item: template,
-      expandable: IS_EXPANDABLE,
-      list,
-      actionButtonType,
-      showInputDefault: false,
-    });
-  });
