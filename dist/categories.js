5466f42c448aed8e86c1d5d2541845b38b2520f2
Dylan Prabagaran <prabagad@myumanitoba.ca>
2025-03-25 12:26:14 -0500

    working on categories

--- dist/categories.js
+++ dist/categories.js
@@ -32,12 +32,36 @@ const list = InitializeList({
     primaryID: user.userID,
     onAddItem: (item) => {
         console.log("item added...", item);
+        const rr = localStorage.getItem("category");
+        let cc;
+        if (rr) {
+            cc = JSON.parse(rr);
+            cc.push(item);
+        }
+        else
+            cc = item;
+        localStorage.setItem("category", JSON.stringify(cc));
     },
     ondeleteItem: (itemID) => {
         console.log("item deleted...", itemID);
+        const rr = localStorage.getItem("category");
+        let cc;
+        if (rr) {
+            cc = JSON.parse(rr);
+            cc = cc.filter((item) => item.itemID !== itemID);
+        }
+        localStorage.setItem("category", JSON.stringify(cc));
     },
     onupdateItem: (item) => {
         console.log("item updated...", item);
+        const rr = localStorage.getItem("category");
+        let cc;
+        if (rr) {
+            cc = JSON.parse(rr);
+            const ind = cc.findIndex((item_) => item_.itemID === item.itemID);
+            cc[ind] = item;
+        }
+        localStorage.setItem("category", JSON.stringify(cc));
     },
 });
 // add the list of categories to the page
@@ -56,3 +80,14 @@ Categories.map((category) => category)
         showInputDefault: false,
     });
 });
+const rr = localStorage.getItem("category");
+let cc;
+if (rr) {
+    cc = JSON.parse(rr);
+    cc.forEach((item) => list.addItem({
+        item,
+        expandable: IS_EXPANDABLE,
+        list,
+        actionButtonType,
+    }));
+}
--- src/categories.ts
+++ src/categories.ts
@@ -1,6 +1,6 @@
 import { InitializeList } from "./components/list.js";
 import { mountPageWrapper } from "./components/pageWrapper.js";
-import { Categories } from "./types/types.js";
+import { Categories, ListItem } from "./types/types.js";
 import { ActionButtonType } from "./types/types";
 import { getUser } from "./utils/getUser.js";
 import { createItemTemplate } from "./utils/createItemTemplate.js";
@@ -37,12 +37,37 @@ const list = InitializeList({
   primaryID: user.userID,
   onAddItem: (item) => {
     console.log("item added...", item);
+    const rr = localStorage.getItem("category");
+    let cc;
+    if (rr) {
+      cc = JSON.parse(rr) as ListItem[];
+      cc.push(item);
+    } else cc = item;
+
+    localStorage.setItem("category", JSON.stringify(cc));
   },
   ondeleteItem: (itemID) => {
     console.log("item deleted...", itemID);
+    const rr = localStorage.getItem("category");
+    let cc;
+    if (rr) {
+      cc = JSON.parse(rr) as ListItem[];
+      cc = cc.filter((item) => item.itemID !== itemID);
+    }
+
+    localStorage.setItem("category", JSON.stringify(cc));
   },
   onupdateItem: (item) => {
     console.log("item updated...", item);
+    const rr = localStorage.getItem("category");
+    let cc;
+    if (rr) {
+      cc = JSON.parse(rr) as ListItem[];
+      const ind = cc.findIndex((item_) => item_.itemID === item.itemID);
+      cc[ind] = item;
+    }
+
+    localStorage.setItem("category", JSON.stringify(cc));
   },
 });
 
@@ -62,3 +87,17 @@ Categories.map((category) => category)
       showInputDefault: false,
     });
   });
+
+const rr = localStorage.getItem("category");
+let cc;
+if (rr) {
+  cc = JSON.parse(rr) as ListItem[];
+  cc.forEach((item) =>
+    list.addItem({
+      item,
+      expandable: IS_EXPANDABLE,
+      list,
+      actionButtonType,
+    })
+  );
+}
